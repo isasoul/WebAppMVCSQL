3352260401d22b101b22cf27a8ddd31f3a79eccb
Isabel Molina <mariaisabelmolinafer@gmail.com>
2023-10-01 23:40:23 -0300

    Agregar archivos de proyecto.

---
+++ WebAppMVCSQL/Data/IContactsRepository.cs
@@ -0,0 +1,15 @@
+﻿using WebAppMVCSQL.Models;
+
+namespace WebAppMVCSQL.Data
+{
+    public interface IContactsRepository
+    {
+        Task<IEnumerable<Contact>> GetAll();
+        Task<Contact> GetDetails(int id);
+        Task Insert (Contact contact);
+        Task Update (Contact contact);
+        Task Delete (int id);
+
+
+    }
+}
